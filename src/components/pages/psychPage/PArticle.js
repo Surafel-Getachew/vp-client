96ade18aafd5e4cf22a060a8c979312bd60ff096
= <=>
2020-03-21 04:25:10 -0700

    everything untill article done

--- src/components/pages/psychPage/PArticle.js
+++ src/components/pages/psychPage/PArticle.js
@@ -1,76 +1,92 @@
-import React,{useState,useContext,useEffect} from "react";
+import React, { useState, useContext, useEffect } from "react";
 
 import ArticleContext from "../../../context/article/articleContext";
-import ArticleItems from "./Components/ArticleItems"
+import ArticleItems from "./Components/ArticleItems";
 
 const PAlert = () => {
+  const articleContext = useContext(ArticleContext);
+  const {
+    addArticle,
+    articles,
+    loadPsychiatristArticles,
+    current,
+    updateArticle,
+    clearCurrent
+  } = articleContext;
 
-    const articleContext = useContext(ArticleContext);
-    const {addArticle,articles,loadPsychiatristArticles,current,updateArticle,clearCurrent} = articleContext;
-    
-    const [article,setArticle] = useState({
-        title:"",
-        body:"",
-        
-    })
+  const [article, setArticle] = useState({
+    title: "",
+    body: ""
+  });
 
-    const {title,body} = article;
+  const { title, body } = article;
 
-    useEffect(() => {
-       loadPsychiatristArticles();
-    },[])
+  useEffect(() => {
+    loadPsychiatristArticles();
+  }, []);
 
-    useEffect(() => {
-        if(current !== null){
-            setArticle(current)
-        }
-        else {
-            setArticle({title:"",body:""})
-        }
-    },[articleContext,current])
+  useEffect(() => {
+    if (current !== null) {
+      setArticle(current);
+    } else {
+      setArticle({ title: "", body: "" });
+    }
+  }, [articleContext, current]);
 
-    const onChange = (e) => {
-        setArticle({...article,[e.target.name]:e.target.value});
+  const onChange = e => {
+    setArticle({ ...article, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (current == null) {
+      addArticle({ title, body });
+    } else {
+      updateArticle(article);
     }
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        if(current == null){
-            addArticle({title,body});
-        }
-        else{
-            updateArticle(article)
-        }
-        
-        clearCurrent();
-    }
+    clearCurrent();
+  };
 
-    // const clearAll =  (current) => {
-    //     clearCurrent();
-    // }
+  // const clearAll =  (current) => {
+  //     clearCurrent();
+  // }
 
-    return (
-        <div>
-            <form onSubmit = {onSubmit}>
-                <label>Title</label> <br/>
-                <input type = "text" name = "title" value = {title} onChange = {onChange} placeholder = "title"/><br/>
-                <label>body</label><br/>
-                <textarea type = "text" name = "body" value = {body} onChange = {onChange} placeholder = "body" rows = "20" cols = "90" />
-                <input type = "submit" value = "post" />
-            </form>
-            
-            <div>
-            {articles.map((article) => (
-                <div key = {article._id}>
-                    <ArticleItems articles = {article} />
-                </div>
-            ))}
-        </div>
-               
-            
-        </div>
-        
-    )
-}
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <label>Title</label> <br />
+        <input
+          type="text"
+          name="title"
+          value={title}
+          onChange={onChange}
+          placeholder="title"
+        />
+        <br />
+        <label>body</label>
+        <br />
+        <textarea
+          type="text"
+          name="body"
+          value={body}
+          onChange={onChange}
+          placeholder="body"
+          rows="20"
+          cols="90"
+        />
+        <input type="submit" value="post" />
+      </form>
+
+      <div>
+        {articles.map(article => (
+          <div key={article._id}>
+            <ArticleItems articles={article} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default PAlert;
