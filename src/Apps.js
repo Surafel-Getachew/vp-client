1fb732ab67deedf6b16b84f8f0fd1b00561dd7a9
= <=>
2020-05-30 01:51:34 -0700

    su functionality

--- src/Apps.js
+++ src/Apps.js
@@ -1,23 +1,29 @@
-import React from 'react'
-import {BrowserRouter,Route,Switch} from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+// context
+import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
 
 // pages
 import Landing from "./pages/Landing/Landing";
-import PsychiatristLanding from "./pages/Psychiatrist/PsychiatristLanding"
+import PsychiatristLanding from "./pages/Psychiatrist/PsychiatristLanding";
 import Signup from "./pages/User/Auth/Signup";
 
 const Apps = () => {
-
-    return (
+  return (
+    <AuthState>
+      <AlertState>
         <BrowserRouter>
-            <Switch>
-                <Route exact path = "/" component = {Landing} />
-                <Route exact path = "/psychiatrist" component = {PsychiatristLanding} />
-                <Route exact path = "/signup" component = {Signup} />
-            </Switch>
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/psychiatrist" component={PsychiatristLanding} />
+            <Route exact path="/signup" component={Signup} />
+          </Switch>
         </BrowserRouter>
-    )
-    
-}
+      </AlertState>
+    </AuthState>
+  );
+};
 
-export default Apps
+export default Apps;
